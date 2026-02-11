da6321970148487db2380e0c7f83535497046cd7
linuxie <linuxie@home.com>
2026-02-11 16:48:07 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   // GitHub Pages 배포를 위한 경로 설정
-  publicPath: process.env.NODE_ENV === "production" ? "/리포지토리이름/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/birthday-bpp/" : "/",
 });
